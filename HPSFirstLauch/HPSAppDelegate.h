1b7a1ed32d8426319535621eb6f85447669135d2
hupeisen <hupeisen@gmail.com>
2013-11-18 15:14:25 +0800

    Initial Commit

---
+++ HPSFirstLauch/HPSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HPSAppDelegate.h
+//  HPSFirstLauch
+//
+//  Created by peisen hups on 13-11-18.
+//  Copyright (c) 2013年 peisen hups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HPSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HPSFirstLauch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HPSFirstLauch
+//
+//  Created by peisen hups on 13-11-18.
+//  Copyright (c) 2013年 peisen hups. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HPSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HPSAppDelegate class]));
+    }
+}
